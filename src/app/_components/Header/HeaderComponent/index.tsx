23a64cd7877c4fb4d7b5800e992900163dbf7388
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-20 23:00:22 +0100

    add logo to HeaderComponent

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
+import Link from 'next/link'
 
 const HeaderComponent = ({ header }: {header: Header }) => {
   return (
     <nav>
       <Gutter>
-        <Link>
-        
+        <Link href="/">
+          <img src="/logo-black.svg" alt="logo" />
         </Link>  
       </Gutter> 
     </nav>

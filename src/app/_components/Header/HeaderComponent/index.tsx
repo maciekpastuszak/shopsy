5afbba9239a9025f6730ecbd4e0a2bcad8f260ac
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-21 12:02:31 +0100

    change img to Image component

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -5,13 +5,14 @@ import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 
 import classes from "./index.module.scss"
+import Image from 'next/image'
 
 const HeaderComponent = ({ header }: {header: Header }) => {
   return (
     <nav className={[classes.header]}>
       <Gutter className={classes.wrap}>
         <Link href="/">
-          <img src="/logo-black.svg" alt="logo" className={classes.logo}/>
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
         </Link>  
       </Gutter> 
     </nav>

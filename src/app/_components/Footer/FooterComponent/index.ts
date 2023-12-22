1c22004fbb5161af0d46f5d13eae7e4b45692fd7
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-22 19:10:28 +0100

    add FooterComponent

---
+++ src/app/_components/Footer/FooterComponent/index.ts
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from './index.module.scss'
+
+const FooterComponent = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default FooterComponent

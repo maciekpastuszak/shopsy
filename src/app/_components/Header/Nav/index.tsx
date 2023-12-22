b9d9b3f547cb315e08bd679aedb8b3bfe2b62b2f
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-22 18:55:45 +0100

    add button props in Nav

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -30,7 +30,13 @@ export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
       })}
       <CartLink />
       {user && <Link href="/account">Account</Link>}
-      {!user && ( <Button />
+      {!user && ( <Button 
+        el="link"
+        href="/login"
+        label="Login"
+        appearance="primary"
+        onClick={() => (window.location.href = '/login')}
+      />
       )}
     </nav>
   )

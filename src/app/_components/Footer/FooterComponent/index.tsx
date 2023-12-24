15f5ed99fd8e8b8d0d86be0f7dd7a12f40c42a33
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-24 22:36:55 +0100

    update globals.ts footer props

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -39,7 +39,7 @@ const FooterComponent = ({ footer }: {footer: Footer}) => {
                 <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
               </Link>
 
-              {/* <p>{footer.copyright}</p> */}
+              <p>{footer.copyright}</p>
             </div>
           </Gutter>
       </div>

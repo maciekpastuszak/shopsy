663830dfb63062fe1cbd843c47412351bb4cf9ce
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-26 16:26:52 +0100

    add welcome img to Login page

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -33,6 +33,16 @@ export default async function Login() {
       <div className={classes.formWrapper}>
         <div className={classes.formContainer}>
           <RenderParams className={classes.params} />
+        
+          <div className={classes.formTitle}>
+            <h3>Welcome</h3>
+            <Image 
+              src="assets/icons/hand.png" 
+              alt="hand"
+              width={30}
+              height={30}
+            />
+          </div>
         </div>
       </div>
    </section>

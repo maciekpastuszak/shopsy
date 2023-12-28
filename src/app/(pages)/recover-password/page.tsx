8a9a320094880d0477faf68c6e438028f1d5cc37
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-28 10:40:21 +0100

    update view of recover password page

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -6,12 +6,44 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className={classes.login}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image 
+            src="/logo-black.svg"
+            alt="logo"
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+
+        
+          <div className={classes.formTitle}>
+            <h3>Welcome</h3>
+            <Image 
+              src="assets/icons/hand.png" 
+              alt="hand"
+              width={30}
+              height={30}
+            />
+          </div>
+
+          <p>Please login here</p>
+
+          <RecoverPasswordForm />
+        </div>
+      </div>
+   </section>
   )
 }
 

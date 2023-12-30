064cd99384da5e988a552f35780308155b50ace7
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-30 20:01:01 +0100

    add slug styles

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -20,6 +20,8 @@ import { Gutter } from '../../_components/Gutter'
 // If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
 export const dynamic = 'force-dynamic'
 
+import classes from './index.module.scss';
+
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
 

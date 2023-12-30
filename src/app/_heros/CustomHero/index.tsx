baa174ab8971d0a4ceb1a7fcb2ae97b312c77ea9
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-30 20:19:46 +0100

    add mediaUrl to CustomHero

--- src/app/_heros/CustomHero/index.tsx
+++ src/app/_heros/CustomHero/index.tsx
@@ -9,9 +9,11 @@ import RichText from '../../_components/RichText'
 import classes from './index.module.scss'
 
 export const CustomHero: React.FC<Page['hero']> = ({ richText, media, links }) => {
+  const mediaUrl = media && typeof media !== 'string' && `${process.env.NEXT_PUBLIC_SERVER_URL}/media/${media.filename}`
+  
   return (
     <section className={classes.hero}>
-      <div className={classes.heroWrapper}>
+      <div className={classes.heroWrapper} style={{ backgroundImage: `url(${mediaUrl})`}}>
         <RichText content={richText} />
         {Array.isArray(links) && links.length > 0 && (
           <ul className={classes.links}>

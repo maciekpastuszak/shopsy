a0d091be69fde6b0ac9b8ce095f0fade54b3089b
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-02 17:16:05 +0100

    add main FilterProvider

--- src/app/_providers/Filter/index.tsx
+++ src/app/_providers/Filter/index.tsx
@@ -1,8 +1,14 @@
 'use client'
 
+import { ReactNode } from 'react';
+
 export const INITIAL_FILTER_DATA = {
     categoryFilters: [],
     setCategoryFilters: () => [],
     sort: '',
     setSort: () => '',
   }
+
+  const FilterContext = createContext(INITIAL_FILTER_DATA)
+
+  export const FilterProvider = ({ children }: { children: React.ReactNode })

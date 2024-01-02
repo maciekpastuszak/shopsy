47d7d73acabf200b7862ceae169161e3a687507c
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-02 17:11:09 +0100

    add payload media to CategoryCard

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 import classes from './index.module.scss'
 import Link from 'next/link'
-import { Category } from '../../../../payload/payload-types'
+import { Category, Media } from '../../../../payload/payload-types'
 
 type CategoryCardProps = {
   category: Category
 }
 const CategoryCard = ({ category }: CategoryCardProps) => {
+  const media = category.media as Media
+  
   return (
-    <Link href="/products" className={classes.card}>
+    <Link href="/products" className={classes.card} style={{backgroundImage: `url(${media.url})`}}>
        <p className={classes.title}>{category.title}</p>
     </Link>
   )

9561d0d5b94096d4619c175cb4f17b22451f1bce
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-09 21:59:56 +0100

    add days param to Promotion comp

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -8,6 +8,13 @@ const Promotion = () => {
         <div className={classes.textBox}>
             <h3 className={classes.title}>Deals of the Month</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus quo in optio! Labore amet pariatur quibusdam deleniti sequi itaque inventore. Expedita quas nisi iure repudiandae dolore recusandae reiciendis accusamus perferendis?</p>
+
+            <ul className={classes.stats}>
+                <li className={classes.statBox}>
+                    <h4>5</h4>
+                    <p>days</p>
+                </li>
+            </ul>
         </div>
     </section>
   )

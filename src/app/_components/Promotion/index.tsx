7e16b7b6ca930ed02c6c1024073f9e51544627e3
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-09 21:58:27 +0100

    add title and header to Promotion

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -6,7 +6,8 @@ const Promotion = () => {
   return (
     <section className={classes.promotion}>
         <div className={classes.textBox}>
-
+            <h3 className={classes.title}>Deals of the Month</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus quo in optio! Labore amet pariatur quibusdam deleniti sequi itaque inventore. Expedita quas nisi iure repudiandae dolore recusandae reiciendis accusamus perferendis?</p>
         </div>
     </section>
   )

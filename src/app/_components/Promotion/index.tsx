04556d30e0730a1aad92b88d2d2029ec362b7eda
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-10 21:47:16 +0100

    add useEffect hook to set up a timer in Promotion

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import classes from './index.module.scss';
 
@@ -11,6 +11,33 @@ const Promotion = () => {
     minutes: 0,
     seconds: 0,
   })
+
+  const targetDate = new Date()
+  targetDate.setDate(targetDate.getDate() + 3)
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      const currentTime = new Date()
+      const timeDifference = Math.max(Number(targetDate) - Number(currentTime), 0)
+
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+
+      setTime({ days, hours, minutes, seconds })
+
+      if (timeDifference === 0) {
+        clearInterval(timerInterval)
+        // You can add code here to handle what happens when the target date is reached.
+      }
+    }, 1000)
+
+    return () => {
+      clearInterval(timerInterval) // Cleanup the interval when the component unmounts.
+    }
+  }, [])
+
 
   return (
     <section className={classes.promotion}>

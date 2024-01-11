006d5ce9a8e12810fc58bf673db766449cd8730d
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-11 10:12:17 +0100

    add useFilter hook to Filters page

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import classes from './index.module.scss';
+import { useFilter } from '../../../_providers/Filter';
 const Filters = () => {
+  const {categoryFilters, sort, setCategoryFilters, setSort} = useFilter();
+
+  const handleCategories = (categoryId: string) => {}
   return (
     <div className={classes.filters}>
       <div>
         <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+
+        </div>
       </div>
     </div>
   )

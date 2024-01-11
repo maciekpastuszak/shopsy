50da29ca399a7948324a54e44e5ca68e3d324422
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-11 09:05:24 +0100

    add Blocks to to Products view

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 import classes from './index.module.scss';
 import { Gutter } from '../../_components/Gutter';
+import Filters from './Filters';
+import { Blocks } from '../../_components/Blocks';
 
 const Products = () => {
   return (
     <div className={classes.coontainer}>
-        <Gutter className={classes.products}></Gutter>
+        <Gutter className={classes.products}>
+          <Filters />
+          <Blocks blocks={layout} disableTopPadding={true} />
+        </Gutter>
     </div>
   )
 }

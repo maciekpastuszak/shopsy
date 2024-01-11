c78ffd3d698784be240e8566e8cfd5e1b66b1baa
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-11 09:09:21 +0100

    add fetchDoc to get the page to Products view

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -4,8 +4,25 @@ import classes from './index.module.scss';
 import { Gutter } from '../../_components/Gutter';
 import Filters from './Filters';
 import { Blocks } from '../../_components/Blocks';
+import { Category, Page } from '../../../payload/payload-types';
+import { fetchDoc } from '../../_api/fetchDoc';
+import { draftMode } from 'next/headers';
 
-const Products = () => {
+const Products = async () => {
+  const { isEnabled: isDraftMode } = draftMode();
+  let page: Page | null = null;
+  let categories: Category[] | null = null;
+
+  try {
+    page = await fetchDoc<Page>({
+      collection: 'pages',
+      slug: 'products',
+      draft: isDraftMode,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
   return (
     <div className={classes.coontainer}>
         <Gutter className={classes.products}>

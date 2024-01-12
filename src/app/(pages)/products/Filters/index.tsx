bbe8cd53c28a51c6984657e344f7e64edf69fe27
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-12 21:44:07 +0100

    add mapping through categories in Filters

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import React from 'react'
 
 import classes from './index.module.scss';
 import { useFilter } from '../../../_providers/Filter';
 import { Category } from '../../../../payload/payload-types';
+import { Checkbox } from 'payload/components/forms';
 
 const Filters = ({ categories }: {categories: Category[]}) => {
   const {categoryFilters, sort, setCategoryFilters, setSort} = useFilter();
@@ -15,6 +18,16 @@ const Filters = ({ categories }: {categories: Category[]}) => {
         <div className={classes.categories}>
           {categories.map((category) => {
             const isSelected = false;
+
+            return (
+              <Checkbox 
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
 
           })}
         </div>

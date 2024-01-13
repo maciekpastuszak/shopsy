32c598cf829b96884716e7eef33b380734c97a4f
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-13 21:32:50 +0100

    bugfix filter provider

--- src/app/_providers/Filter/index.tsx
+++ src/app/_providers/Filter/index.tsx
@@ -17,19 +17,21 @@ export const INITIAL_FILTER_DATA = {
   const FilterContext = createContext<IContextType>(INITIAL_FILTER_DATA)
 
   export const FilterProvider = ({ children }: { children: React.ReactNode }) => {
-
-    const [categoryFilters, setCategoryFilters] = useState([]);
-    const [sort, setSort] = useState('-createdAt');
+    const [categoryFilters, setCategoryFilters] = useState([])
+    const [sort, setSort] = useState('-createdAt')
+  
     return (
-      <FilterContext.Provider value={{
-        categoryFilters,
-        setCategoryFilters,
-        sort,
-        setSort
-      }}>
+      <FilterContext.Provider
+        value={{
+          categoryFilters,
+          setCategoryFilters,
+          sort,
+          setSort,
+        }}
+      >
         {children}
       </FilterContext.Provider>
     )
   }
-
-  export const useFilter = () => { useContext(FilterContext)}
+  
+  export const useFilter = () => useContext(FilterContext)

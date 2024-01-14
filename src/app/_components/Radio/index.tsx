74709a82ef5150d34255b383bc061a9aee83fb69
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-14 22:29:15 +0100

    add interface to Radiobutton component

--- src/app/_components/Radio/index.tsx
+++ src/app/_components/Radio/index.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 import classes from './index.module.scss'
 
-export const RadioButton = ({
+interface RadioButtonProps {
+  label: string
+  value: string
+  isSelected: boolean
+  onRadioChange: (value: string) => void
+  groupName: string
+}
+
+export const RadioButton: React.FC<RadioButtonProps> = ({
 }) => {
   
 

b47c270395f3c426efa7a0a1e2bea8c749fd083f
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-23 21:01:00 +0100

    add styles to cart container

--- src/app/(pages)/cart/page.tsx
+++ src/app/(pages)/cart/page.tsx
@@ -57,12 +57,13 @@ export default async function Cart() {
   }
 
   return (
-    <Fragment>
-      <Gutter>
+    <div>
+      <Gutter className={classes.container}>
+        <h3>Cart</h3>
         <CartPage settings={settings} page={page} />
       </Gutter>
-      <Blocks blocks={page?.layout} />
-    </Fragment>
+      <Blocks blocks={page?.layout} disableBottomPadding />
+    </div>
   )
 }
 

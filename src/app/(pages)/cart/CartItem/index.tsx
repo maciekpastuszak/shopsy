5c5e35a40d1c051fc67d84ba5cbf0d3a8a8827e0
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-24 09:57:49 +0100

    add CartItem props

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import classes from './index.module.scss'
 
-const CartItem = () => {
+const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
   return (
-    <li className={classes.item}>
+    <li className={classes.item} key={title}>
       <h6>TITLE</h6>
     </li>
   )

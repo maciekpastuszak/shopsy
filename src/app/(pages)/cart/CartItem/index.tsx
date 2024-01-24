c627faed2a6fc895013c90185a65febe45ece0ac
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-24 09:52:19 +0100

    return a simple view from CartItem

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import classes from './index.module.scss'
+
 const CartItem = () => {
   return (
-    <div>CartItem</div>
+    <li className={classes.item}>
+      <h6>TITLE</h6>
+    </li>
   )
 }
 

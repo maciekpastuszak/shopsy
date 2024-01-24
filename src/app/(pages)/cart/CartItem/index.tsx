825fae89a6a95ffc9154de1082ec9cf25980ffbd
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-24 10:01:42 +0100

    add empty functions to CartItem

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -6,6 +6,11 @@ import classes from './index.module.scss'
 
 const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
   const [quantity, setQuantity] = useState(qty)
+ 
+  const decrementQty = () => {}
+  const incrementQty = () => {}
+  const enterQty = () => {}
+  
   return (
     <li className={classes.item} key={title}>
       <h6>TITLE</h6>

f96948077e999e90f761ccdbe61c87d077e55930
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-24 10:16:42 +0100

    add return Link in CartItem

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react'
 
 import classes from './index.module.scss'
+import { Media } from '../../../_components/Media'
+import Link from 'next/link'
 
 const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
   const [quantity, setQuantity] = useState(qty)
@@ -10,10 +12,15 @@ const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
   const decrementQty = () => {}
   const incrementQty = () => {}
   const enterQty = () => {}
-  
+
   return (
     <li className={classes.item} key={title}>
-      <h6>TITLE</h6>
+      <Link href={`/prodcuts/${product.slug}`} className={classes.mediaWrapper}>
+        {!metaImage && <span>No image</span>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media className={classes.media}/>
+        )}
+      </Link>
     </li>
   )
 }

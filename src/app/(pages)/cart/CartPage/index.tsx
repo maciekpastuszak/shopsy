336de1e668a6845d24e035b8e01fc0dffa6110d9
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-26 13:16:03 +0100

    add summary row header to CartPage

--- src/app/(pages)/cart/CartPage/index.tsx
+++ src/app/(pages)/cart/CartPage/index.tsx
@@ -94,6 +94,10 @@ export const CartPage: React.FC<{
                 return null
               })}
               <div className={classes.summary}>
+                <div className={classes.row}>
+                  <h6 className={classes.cartTotal}>Summary</h6>
+                </div>
+                
                 <Button
                   className={classes.checkoutButton}
                   href={user ? '/checkout' : '/login?redirect=%2Fcheckout'}

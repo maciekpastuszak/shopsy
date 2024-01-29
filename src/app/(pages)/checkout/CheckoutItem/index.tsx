337491766ab118cc4dbd74acdf51f6b3e42b2e68
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-30 00:04:10 +0100

    typo

--- src/app/(pages)/checkout/CheckoutItem/index.tsx
+++ src/app/(pages)/checkout/CheckoutItem/index.tsx
@@ -11,7 +11,7 @@ export const CheckoutItem = ({ product, title, metaImage, quantity, index }) => 
         <Link href={`/products/${product.slug}`} className={classes.mediaWrapper}>
             {!metaImage && <span>No image</span>}
             {metaImage && typeof metaImage !== 'string' && (
-            <Media className={classes.media} imgclassName={classes.image} resource={metaImage} fill />
+            <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill />
             )}
       </Link>
     </li>

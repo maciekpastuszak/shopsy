beddb1539515bb5d2862bc70c90549d9d16cf3ea
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-30 00:03:52 +0100

    ink with metaimg logic to CheckoutItem

--- src/app/(pages)/checkout/CheckoutItem/index.tsx
+++ src/app/(pages)/checkout/CheckoutItem/index.tsx
@@ -8,6 +8,12 @@ import classes from './index.module.scss'
 export const CheckoutItem = ({ product, title, metaImage, quantity, index }) => {
   return (
     <li className={classes.item} key={index}>
+        <Link href={`/products/${product.slug}`} className={classes.mediaWrapper}>
+            {!metaImage && <span>No image</span>}
+            {metaImage && typeof metaImage !== 'string' && (
+            <Media className={classes.media} imgclassName={classes.image} resource={metaImage} fill />
+            )}
+      </Link>
     </li>
   )
 }

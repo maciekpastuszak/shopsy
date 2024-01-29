e37a169b9479565761d972c087b9707ea52a3131
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-30 00:01:44 +0100

    add imports to CheckoutItem

--- src/app/(pages)/checkout/CheckoutItem/index.tsx
+++ src/app/(pages)/checkout/CheckoutItem/index.tsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import Link from 'next/link'
+
+import { Media } from '../../../_components/Media'
+import { Price } from '../../../_components/Price'
+
+import classes from './index.module.scss'
 
 const CheckoutItem = () => {
   return (

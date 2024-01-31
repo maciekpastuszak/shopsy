3e60a0557afe2c69c584f25e767ea176d46a365c
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-31 23:59:26 +0100

    add imports to CheckoutPage

--- src/app/(pages)/checkout/CheckoutPage/index.tsx
+++ src/app/(pages)/checkout/CheckoutPage/index.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+'use client'
+
+import React, { Fragment, useEffect } from 'react'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const CheckoutPage = () => {
   return (
